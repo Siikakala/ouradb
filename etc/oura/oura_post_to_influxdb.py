b064730f1c55ccddf907053b4df54d5aa9bccc13
Mira Ojamo <1149108+Siikakala@users.noreply.github.com>
2023-12-18 00:27:16 +0200

    INFLUX_DB is irrelevant - there's buckets

--- etc/oura/oura_post_to_influxdb.py
+++ etc/oura/oura_post_to_influxdb.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
-import influxdb_client, os, time
+import influxdb_client
+import os
+import time
 from influxdb_client import WritePrecision, InfluxDBClient, Point
 from influxdb_client.client.write_api import SYNCHRONOUS
 import requests
@@ -7,23 +9,25 @@ import argparse
 import json
 import re
 
-#Influxdb2 info
-INFLUXDB_TOKEN = open('/run/secrets/InfluxDBToken','r').read(88)
-INFLUXDB_DB = os.environ["INFLUXDB_DB"]
+# Influxdb2 info
+INFLUXDB_TOKEN = open('/run/secrets/InfluxDBToken', 'r').read(88)
 INFLUXDB_BUCKET = os.environ["BUCKET"]
 INFLUXDB_URL = os.environ["INFLUXDB_URL"]
 INFLUXDB_ORG = os.environ["INFLUXDB_ORG"]
-client_ouradb = influxdb_client.InfluxDBClient(url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
+client_ouradb = influxdb_client.InfluxDBClient(
+    url=INFLUXDB_URL, token=INFLUXDB_TOKEN, org=INFLUXDB_ORG)
 write_api = client_ouradb.write_api(write_options=SYNCHRONOUS)
 
-OURA_PAT = open('/run/secrets/OuraApiToken','r').read(32)
+OURA_PAT = open('/run/secrets/OuraApiToken', 'r').read(32)
 
 
 def fetch_data(start, end, datatype, pat_data):
     url = f"https://api.ouraring.com/v2/usercollection/{datatype}"
     headers = {"Authorization": f"Bearer {pat_data}"}
-    params = {"start_date": f"{start.strftime('%Y-%m-%d')}", 'end_date': f"{end.strftime('%Y-%m-%d')}"}
-    response = requests.request('GET', url, headers=headers, params=params).json()
+    params = {
+        "start_date": f"{start.strftime('%Y-%m-%d')}", 'end_date': f"{end.strftime('%Y-%m-%d')}"}
+    response = requests.request(
+        'GET', url, headers=headers, params=params).json()
 
     if not response["data"]:
         print("No {} data yet for time window, exiting".format(datatype))
@@ -31,7 +35,7 @@ def fetch_data(start, end, datatype, pat_data):
 
     resp = response["data"][0]
 
-    #If we're looking for sleep...cycles through the items in response dictionary, finds the one that contains long_sleep, sets the active resp to that section. Otherwise naps make amess of the data.
+    # If we're looking for sleep...cycles through the items in response dictionary, finds the one that contains long_sleep, sets the active resp to that section. Otherwise naps make amess of the data.
     indexstart = 0
     indexceiling = len(response["data"]) - 1
     if datatype == 'sleep':
@@ -40,30 +44,29 @@ def fetch_data(start, end, datatype, pat_data):
             for k, v in resp2.items():
                 if v == "long_sleep":
                     resp = resp2
-            indexstart+= 1
+            indexstart += 1
 
-    #Adds the contributors section at level 0 of our readiness json. Includes stats like hrv and sleep balance
+    # Adds the contributors section at level 0 of our readiness json. Includes stats like hrv and sleep balance
     if datatype == 'daily_readiness':
         resp2 = response["data"][0]["contributors"]
         resp.pop('contributors', None)
         resp.update(resp2)
 
     # All data should be consistent in influxdb, so turn ints to floats
-    resp = {k:float(v) if type(v) == int else v for k,v in resp.items()}
+    resp = {k: float(v) if type(v) == int else v for k, v in resp.items()}
     return resp
 
-def get_data_one_day(date,pat):
-    end_date=datetime.strptime(date,'%Y-%m-%d')
-    start_date=end_date - timedelta(days=1)
 
+def get_data_one_day(date, pat):
+    end_date = datetime.strptime(date, '%Y-%m-%d')
+    start_date = end_date - timedelta(days=1)
 
-    sleep_data = fetch_data(start_date,end_date,'sleep',pat)
-    readiness_data = fetch_data(start_date,end_date,'daily_readiness',pat)
+    sleep_data = fetch_data(start_date, end_date, 'sleep', pat)
+    readiness_data = fetch_data(start_date, end_date, 'daily_readiness', pat)
 
     if sleep_data == None:
         print("No data, exiting")
         exit()
-
 
     # Clean out array type data
     sleep_data.pop('heart_rate', None)
@@ -75,23 +78,20 @@ def get_data_one_day(date,pat):
     sleep_data.pop('readiness', None)
     readiness_data.pop('contributors', None)
 
-
-
-
-
     # Merge sleep and readiness data
     data = sleep_data
     data.update(readiness_data)
 
     post_data = [{"measurement": "oura_measurements",
-             "time": data['bedtime_end'],
-             "fields": data
-    },]
+                  "time": data['bedtime_end'],
+                  "fields": data
+                  },]
 
     return post_data
 
 
-parser = argparse.ArgumentParser(description='Post Oura data to Influxdb. Omit --start and --end to process data only for today.')
+parser = argparse.ArgumentParser(
+    description='Post Oura data to Influxdb. Omit --start and --end to process data only for today.')
 parser.add_argument('--start', help="Start date of query. Format: YYYY-MM-DD")
 parser.add_argument('--end', help="End date of query. Format: YYYY-MM-DD")
 args = parser.parse_args()
@@ -119,16 +119,15 @@ if not args.start and not args.end:
     end_date = start_date + timedelta(days=1)
     only_today = True
 else:
-    start_date = datetime.strptime(args.start,'%Y-%m-%d') + timedelta(days=1)
-    end_date = datetime.strptime(args.end,'%Y-%m-%d') + timedelta(days=1)
+    start_date = datetime.strptime(args.start, '%Y-%m-%d') + timedelta(days=1)
+    end_date = datetime.strptime(args.end, '%Y-%m-%d') + timedelta(days=1)
 
 
 # Go through all days between start and end dates
 
 while start_date <= end_date:
-    data = get_data_one_day(end_date.strftime('%Y-%m-%d'),OURA_PAT)
+    data = get_data_one_day(end_date.strftime('%Y-%m-%d'), OURA_PAT)
     write_api.write(bucket=INFLUXDB_BUCKET, org=INFLUXDB_ORG, record=data)
     print(end_date)
-    #print(json.dumps(data, indent=4))
+    # print(json.dumps(data, indent=4))
     end_date = end_date - timedelta(days=1)
-
